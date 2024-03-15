3e515346dff6829e80e7f15ad6bba5154b810d0f
Arnau Diaz <diazarnau0@gmail.com>
2024-03-15 13:38:04 +0100

    zones

--- ugly-vpc.tf
+++ ugly-vpc.tf
@@ -2,23 +2,43 @@
 
 # main vpc
 
+variable "availability_zones" {
+  type    = list(string)
+  default = ["a", "b", "c"]
+}
+
 resource "aws_vpc" "ugly_org" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "UglyOrg"
+    Name = "UglyOrgVPC"
   }
 }
 
-resource "aws_subnet" "ugly_org_subnet" {
+# Create public subnets
+resource "aws_subnet" "ugly_org_public" {
+  count                   = length(var.availability_zones)
   vpc_id                  = aws_vpc.ugly_org.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "${var.region}a"
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  availability_zone       = "${var.region}${var.availability_zones[count.index]}"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "UglyOrg"
+    Name = "UglyOrgVPCPublic${var.availability_zones[count.index]}"
   }
 }
+
+# Create private subnets
+resource "aws_subnet" "ugly_org_private" {
+  count             = length(var.availability_zones)
+  vpc_id            = aws_vpc.ugly_org.id
+  cidr_block        = "10.0.${count.index + 101}.0/24"
+  availability_zone = "${var.region}${var.availability_zones[count.index]}"
+
+  tags = {
+    Name = "UglyOrgVPCPrivate${var.availability_zones[count.index]}"
+  }
+}
+
