9f8a278dd0a714c9d61aa7b4e75df1ac1eb32323
Arnau Diaz <diazarnau0@gmail.com>
2024-03-15 12:54:37 +0100

    fmt

--- ugly-vpc.tf
+++ ugly-vpc.tf
@@ -3,8 +3,8 @@
 # main vpc
 
 resource "aws_vpc" "ugly_org" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
